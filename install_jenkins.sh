9567aa77fb91fb3db95fe8d3acf815dcb1dbf79d
Vsevolod <147947186+sevii-ia@users.noreply.github.com>
2025-02-01 11:41:44 +0100

    Update and rename install jenkins to install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,10 +4,10 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install -y jenkins
 
 sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install -y fontconfig openjdk-17-jre
 java -version
 openjdk version "17.0.8" 2023-07-18
 OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
